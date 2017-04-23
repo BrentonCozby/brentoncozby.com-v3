0392802442c6849a0e086b3cfd8f04cc366db83f
Brenton Cozby <brenton.cozby@gmail.com>
2017-04-23 12:44:54 -0400

    add projects

--- src/data.js
+++ src/data.js
@@ -1,51 +1,65 @@
 // title, description, png-src, gif-src, demo-link, code-link
-const creations = [
-
-];
+const creations = [];
 
 // title, description, png-src, gif-src, demo-link, code-link
 const creationsFeatured = [
     {
-        title: 'tech.fvi.edu',
-        description: 'Vanilla-JS SPA for the Techlaunch Coding School at the Florida Vocational Institute.',
-        'png': 'images/tech.fvi.edu.png',
-        'gif': 'images/tech.fvi.edu.png',
-        'demo': 'http://tech.fvi.edu/#/web-developer-program',
-        'code': 'https://github.com/FVITech/tech-fvi-edu'
-    },
-    {
-        title: 'Quote Collage',
-        description: 'Organize your favorite quotes in a beautiful Pinterest-style layout.',
-        'png': 'https://i.imgur.com/Jc6GlD6.png',
-        'gif': 'https://i.imgur.com/FM2GSEV.gif',
-        'demo': 'https://brentoncozby.com/projects/quote-collage/',
-        'code': 'https://github.com/BrentonCozby/quote-collage'
-    },
-    {
+        title: 'Rally Point Webinars',
+        description: 'Website for Rally Point Webinars. Uses Webpack, SASS, Pug, custom animations, and jQuery.',
+        'png': 'images/rally-point-webinars.jpg',
+        'gif': 'images/rally-point-webinars.jpg',
+        'demo': 'http://thinkrallypointwebinars.com',
+        'code': 'https://github.com/BrentonCozby/rally-point-webinars'
+    }, {
+        title: 'FVI Continuing Eduation',
+        description: 'Website for Continuing Eduation department at the Florida Vocational Institute.',
+        'png': 'images/continuing-education.jpg',
+        'gif': 'images/continuing-education.jpg',
+        'demo': 'https://fvitech.github.io/continuing-education',
+        'code': 'https://github.com/fvitech/continuing-education'
+    }, {
         title: 'Lumber Calculator',
         description: 'Optimizes lumber orders and generates cut lists for various City Year Care Force schematics.',
         'png': 'https://i.imgur.com/mI78pXY.jpg',
         'gif': 'https://i.imgur.com/e52JDDd.gif',
         'demo': 'https://brentoncozby.com/projects/CareForce-Lumber-Calculator/',
         'code': 'https://github.com/BrentonCozby/CareForce-Lumber-Calculator'
-    },
-    {
+    }, {
+        title: 'Recipe App',
+        description: 'React application that uses Firebase for the client-side database.',
+        'png': 'https://i.imgur.com/mI78pXY.jpg',
+        'gif': 'https://i.imgur.com/e52JDDd.gif',
+        'demo': 'https://brentoncozby.com/projects/FreeCodeCamp/fcc-recipe-app/',
+        'code': 'https://github.com/BrentonCozby/fcc-recipe-app'
+    }, {
+        title: 'Youtube Clone',
+        description: 'Youtube created with react, SASS, Webpack, and the Youtube API.',
+        'png': 'https://i.imgur.com/mI78pXY.jpg',
+        'gif': 'https://i.imgur.com/e52JDDd.gif',
+        'demo': 'https://brentoncozby.github.io/youtube-react',
+        'code': 'https://github.com/BrentonCozby/youtube-react'
+    }, {
+        title: 'Quote Collage',
+        description: 'Organize your favorite quotes in a beautiful Pinterest-style layout.',
+        'png': 'https://i.imgur.com/Jc6GlD6.png',
+        'gif': 'https://i.imgur.com/FM2GSEV.gif',
+        'demo': 'https://brentoncozby.com/projects/quote-collage/',
+        'code': 'https://github.com/BrentonCozby/quote-collage'
+    }, {
+        title: 'tech.fvi.edu',
+        description: 'Vanilla-JS SPA for the Techlaunch Coding School at the Florida Vocational Institute.',
+        'png': 'images/tech.fvi.edu.png',
+        'gif': 'images/tech.fvi.edu.png',
+        'demo': 'http://tech.fvi.edu/#/web-developer-program',
+        'code': 'https://github.com/FVITech/tech-fvi-edu'
+    }, {
         title: 'Image Search API',
         description: 'Returns JSON data for images based on the query string parameters.',
         'png': 'images/image-search-api.png',
         'gif': 'images/image-search-api.png',
         'demo': 'https://brentoncozby.com/projects/FreeCodeCamp/image-search-fcc-cozby/',
         'code': 'https://github.com/BrentonCozby/image-search-fcc-cozby'
-    },
-    {
-        title: 'User Agent Data API',
-        description: 'Returns client\'s IP address, OS, brower data, engine, and CPU.',
-        'png': 'images/user-agent-data.png',
-        'gif': 'images/user-agent-data.png',
-        'demo': 'https://req-header-parse-fcc-cozby.herokuapp.com/',
-        'code': 'https://github.com/BrentonCozby/req-header-parse-fcc-cozby'
-    },
-    {
+    }, {
         title: 'Simon Game from the 80s',
         description: 'It really is that simon game from the 80s.',
         'png': 'images/simon-game.png',
@@ -77,6 +91,6 @@ const images = {
     'images/too-big-for-cart.jpg': 'Brenton Cozby too big for that cart. And too big to be pushed in one.'
 };
 
-export { images }
-export { creations }
-export { creationsFeatured }
+export {images}
+export {creations}
+export {creationsFeatured}
